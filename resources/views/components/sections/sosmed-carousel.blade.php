241fdabe0f7d48d361812fc2c16a8cd3f7c829bd
Robby Hernowo <robbyhernowo@gmail.com>
2025-11-04 00:33:58 +0700

    ini dia

--- resources/views/components/sections/sosmed-carousel.blade.php
+++ resources/views/components/sections/sosmed-carousel.blade.php
@@ -86,12 +86,12 @@
                                 </div>
 
                                 {{-- badges --}}
-                                <div class="absolute top-3 left-4 text-[12px] font-medium text-white/90 z-10">
+                                {{-- <div class="absolute top-3 left-4 text-[12px] font-medium text-white/90 z-10">
                                     {{ $p['category'] ?? '' }}
                                 </div>
                                 <div class="absolute top-3 right-4 text-[12px] text-white/80 z-10">
                                     {{ $p['date'] ?? '' }}
-                                </div>
+                                </div> --}}
 
                                 {{-- caption --}}
                                 <div class="absolute left-0 right-0 bottom-0 p-4 text-white z-10">
